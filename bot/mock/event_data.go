d90a56eaf1130e655038d912211a29274a40029a
Omri Harel <omriharel@users.noreply.github.com>
2017-07-16 17:34:20 +0300

    add description to mockEventData

--- bot/mock/event_data.go
+++ bot/mock/event_data.go
@@ -8,6 +8,7 @@ import (
 type MockEventData struct {
 	Number           int
 	Title            string
+	Description      string
 	State            string
 	Owner            string
 	Repo             string
@@ -37,6 +38,10 @@ func (m *MockEventData) GetNumber() int {
 }
 
 func (m *MockEventData) GetTitle() string {
+	return m.Title
+}
+
+func (m *MockEventData) GetDescription() string {
 	return m.Title
 }
 
