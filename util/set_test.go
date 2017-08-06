07108353830d6e965ebedb4ed45a37f75a6749b9
Eliran Bivas <eliran.bivas@gmail.com>
2017-08-06 21:27:01 +0300

    string set test

---
+++ util/set_test.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSet(t *testing.T) {
+	values := []string{"one", "one", "two"}
+	set := StringSet{}
+	set.AddAll(values)
+	result := set.Values()
+	assert.Len(t, result, 2, "2 items")
+	assert.Contains(t, result, "one", "one")
+	assert.Contains(t, result, "two", "two")
+}
+
+func TestSetWithTransform(t *testing.T) {
+	values := []string{"ONE", "ONE", "TwO"}
+	set := StringSet{Transformer: strings.ToLower}
+	set.AddAll(values)
+	result := set.Values()
+	assert.Len(t, result, 2, "2 items")
+	assert.Contains(t, result, "one", "one")
+	assert.Contains(t, result, "two", "two")
+}
