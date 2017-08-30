ea126acbe0d0a9d50f1c3f9610ecfca98a4a020e
Eliran Bivas <eliran.bivas@gmail.com>
2017-08-30 23:35:35 +0300

    load rules from RO data

--- runner/env/env.go
+++ runner/env/env.go
@@ -1,19 +1,12 @@
 package env
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/bivas/rivi/types"
 	"github.com/bivas/rivi/util/log"
-
-	"gopkg.in/src-d/go-billy.v3/osfs"
-	"gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/plumbing"
-	"gopkg.in/src-d/go-git.v4/storage/filesystem"
 )
 
 const (
@@ -41,39 +34,6 @@ func (e *tempFSEnvironment) ConfigFilePath() string {
 	return e.rulesFile
 }
 
-func (e *tempFSEnvironment) clone(data types.ReadOnlyData) error {
-	fs := osfs.New(e.dir)
-	p, _ := filesystem.NewStorage(fs)
-	e.logger.DebugWith(log.MetaFields{
-		log.F("issue", data.GetShortName()),
-		log.F("ref", data.GetOrigin().Ref),
-		log.F("url", data.GetOrigin().GitURL),
-		log.F("hash", data.GetOrigin().Head),
-	}, "cloning repository")
-	repo, err := git.Clone(p, fs, &git.CloneOptions{
-		URL:               data.GetOrigin().GitURL,
-		ReferenceName:     plumbing.ReferenceName("refs/heads/" + data.GetOrigin().Ref),
-		SingleBranch:      true,
-		Depth:             1,
-		RecurseSubmodules: git.NoRecurseSubmodules,
-		Progress:          nil,
-	})
-	if err != nil {
-		return err
-	}
-	head, err := repo.Head()
-	if err != nil {
-		return err
-	}
-	if !strings.HasPrefix(head.Hash().String(), data.GetOrigin().Head) {
-		return fmt.Errorf(
-			"Head Ref (%s) and Origin Ref (%s) do not match",
-			head.Hash().String()[:6],
-			data.GetOrigin().Head)
-	}
-	return nil
-}
-
 func (e *tempFSEnvironment) Create(data types.ReadOnlyData) error {
 	temp, err := ioutil.TempDir("", "rivi-env-")
 	if err != nil {
@@ -83,11 +43,11 @@ func (e *tempFSEnvironment) Create(data types.ReadOnlyData) error {
 		log.F("issue", data.GetShortName()),
 		log.F("dir", temp)}, "Created temp dir")
 	e.dir = temp
-	if err := e.clone(data); err != nil {
-		return err
-	}
 	rules := filepath.Join(e.dir, RULES_CONFIG_FILE)
-	if _, err := os.Stat(rules); err != nil {
+	if err := ioutil.WriteFile(
+		rules,
+		[]byte(data.GetRepository().GetRulesFile()),
+		0400); err != nil {
 		return err
 	}
 	e.rulesFile = rules
