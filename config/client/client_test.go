1cf2426bcfacf78c7aee5c702168a3cc671076be
Eliran Bivas <eliran.bivas@gmail.com>
2017-09-16 19:43:37 +0300

    add test coverage for client config

--- config/client/client_test.go
+++ config/client/client_test.go
@@ -1,10 +1,11 @@
 package client
 
 import (
-	"github.com/spf13/viper"
-	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestClientConfigFromEnv(t *testing.T) {
@@ -24,4 +25,14 @@ func TestConfigTest(t *testing.T) {
 	config := NewClientConfig(v)
 	assert.Equal(t, "token-from-viper", config.GetOAuthToken(), "token from viper")
 	assert.Equal(t, "secret-from-viper", config.GetSecret(), "secret from viper")
+}
+
+func TestConfigFromDevNullFile(t *testing.T) {
+	config := NewClientConfigFromFile("/dev/null")
+	assert.Empty(t, config.GetSecret(), "no secret")
+}
+
+func TestConfigFromFileWithConfigPart(t *testing.T) {
+	config := NewClientConfigFromFile("client_config_test.yml")
+	assert.Equal(t, "github-token", config.GetOAuthToken(), "token")
 }
