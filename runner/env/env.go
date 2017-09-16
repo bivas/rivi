4f6cf50c03d6b4c664628407daa7299db9311a6a
Eliran Bivas <eliran.bivas@gmail.com>
2017-09-16 21:52:42 +0300

    add test for temp fs

--- runner/env/env.go
+++ runner/env/env.go
@@ -1,10 +1,6 @@
 package env
 
 import (
-	"io/ioutil"
-	"os"
-	"path/filepath"
-
 	"github.com/bivas/rivi/types"
 	"github.com/bivas/rivi/util/log"
 )
@@ -17,48 +13,6 @@ type Environment interface {
 	Create(types.ReadOnlyData) error
 	Cleanup() error
 	ConfigFilePath() string
-}
-
-type tempFSEnvironment struct {
-	dir       string
-	rulesFile string
-
-	logger log.Logger
-}
-
-func (e *tempFSEnvironment) ConfigFilePath() string {
-	return e.rulesFile
-}
-
-func (e *tempFSEnvironment) Create(data types.ReadOnlyData) error {
-	temp, err := ioutil.TempDir("", "rivi-env-")
-	if err != nil {
-		return err
-	}
-	e.logger.DebugWith(log.MetaFields{
-		log.F("issue", data.GetShortName()),
-		log.F("dir", temp)}, "Created temp dir")
-	e.dir = temp
-	rules := filepath.Join(e.dir, types.RulesConfigFileName)
-	if err := ioutil.WriteFile(
-		rules,
-		[]byte(data.GetRepository().GetRulesFile()),
-		0600); err != nil {
-		return err
-	}
-	e.rulesFile = rules
-	return nil
-}
-
-func (e *tempFSEnvironment) Cleanup() error {
-	e.logger.DebugWith(
-		log.MetaFields{
-			log.F("dir", e.dir)}, "Cleanup temp dir")
-	return os.RemoveAll(e.dir)
-}
-
-func tempFSEnvironmentProvider() Environment {
-	return &tempFSEnvironment{logger: le.Get("temp")}
 }
 
 type EnvironmentProvider func() Environment
