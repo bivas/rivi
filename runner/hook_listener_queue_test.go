1f3e771d9416e754c6243f60ee992a94ee125f8f
Eliran Bivas <eliran.bivas@gmail.com>
2017-09-23 17:06:18 +0300

    add more test to hook listener

---
+++ runner/hook_listener_queue_test.go
@@ -0,0 +1,30 @@
+package runner
+
+import (
+	"testing"
+
+	"github.com/bivas/rivi/runner/types"
+	"github.com/stretchr/testify/assert"
+)
+
+type testHookListenerQueue struct {
+	Created bool
+}
+
+func (*testHookListenerQueue) Enqueue(*types.Message) {
+	panic("implement me")
+}
+
+func TestSetHookListenerQueueProvider(t *testing.T) {
+	queue := &testHookListenerQueue{false}
+	SetHookListenerQueueProvider(func() types.HookListenerQueue {
+		queue.Created = true
+		return queue
+	})
+	assert.False(t, queue.Created, "created should be false")
+	result := CreateHookListenerQueue()
+	assert.NotNil(t, result, "test result")
+	_, ok := result.(*testHookListenerQueue)
+	assert.True(t, ok, "same type")
+	assert.True(t, queue.Created, "created")
+}
